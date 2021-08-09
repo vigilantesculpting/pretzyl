4d4f7b36b21e214d9028f02fbec7821c78b76615
g0rb <vigilantesculpting@gmail.com>
2021-08-08 21:29:11 -0700

    Modified test.py to suit the refactored operator code

--- test.py
+++ test.py
@@ -9,139 +9,177 @@ import pdb
 #######################################################################
 # Some simple unit test code follows:
 
+def testexpr(p, expr, expected = [True], tolist = False, Exc = None):
+	print "-------------------------"
+	if Exc is None:
+		print "testing expr <%s>, tolist %s, expected <%s>" % (expr, tolist, expected)
+	else:
+		print "testing expr <%s>, tolist %s, exception %s" % (expr, tolist, Exc)
+	try:
+		d = p.eval(expr, count = None)
+	except Exception as e:
+		assert(Exc is not None and isinstance(e, Exc))
+		print "got expected exception", Exc
+		return True
+	if tolist:
+		d = [list(i) for i in d]
+	assert(d == expected)
+	print "got expected result", expected
+
 def test():
+
+	#pretzyl.LOG = True
 
 	env = {
 		'name': 'Jack',
 		'key': 'a7c34bd',
+		'setbutFalse': False,
+		'testlist': ['one', 'two', 'three'],
+		'testdict': {'one': 'ONE', 'two': 'TWO', 'three': 'THREE'},
 	}
 
 	p = pretzyl.Pretzyl(env)
 
-	d = p.eval("('jack' 'suzy' 'mary') 2 <>")
-	print d
-	assert(d == ["mary"])
+	# test simple unary operators
 
-	d = p.eval("('jack' 'suzy' 'mary') 1 None []")
-	d = list(d)
-	print d
-	assert(d == [['suzy', 'mary']])
+	# -
+	testexpr(p, "True", [True])
+	testexpr(p, "False", [False])
+	testexpr(p, "1", [1])
+	testexpr(p, "1 2 3", [1, 2, 3])
+	testexpr(p, "(1 2 3)", [[1, 2, 3]])
+	testexpr(p, "3.14", [3.14])
 
-	# a simple arithmetic expression
-	d = p.eval("2 2 2 sum 4 *")
-	print d
-	assert(d == [24])
+	# exists
+	testexpr(p, "jack exists", [False])
+	testexpr(p, "setbutFalse exists", [True])
+	testexpr(p, "name exists", [True])
 
-	# a nested arithmetic expression
-	d = p.eval("2 (2 2 sum) 4 *")
-	print d
-	assert(d == [16])
+	# not
+	testexpr(p, "name not", [False])
+	testexpr(p, "setbutFalse not", [True])
+	testexpr(p, "False not", [True])
+	testexpr(p, "True not", [False])
+	testexpr(p, "None not", [True])
 
-	# more arithmetic...
-	d = p.eval("4 *2")
-	print d
-	assert(d == [16])
+	# isnone
+	testexpr(p, "None isnone", [True])
+	testexpr(p, "True isnone", [False])
+	testexpr(p, "False isnone", [False])
 
-	d = p.eval("4 4 **")
-	print d
-	assert(d == [256])
+	# makeref
+	testexpr(p, "'n' 'ame' + makeref", ["Jack"])
 
-	# string manipulation
-	d = p.eval("   'hello [' name  ']!' + 2 times")
-	print d
-	assert(d == ["hello [Jack]!"])
+	# makeop
+	testexpr(p, "4 5 'sum' makeop", [9])
 
-	# basic booleans
-	d = p.eval("True")
-	print d
-	assert(d == [True])
+	# length
+	testexpr(p, "(1 2 3) length", [3])
+	testexpr(p, "'hello world!' length", [12])
 
-	# more booleans
-	d = p.eval("5 4 <")
-	print d
-	assert(d == [False])
+	# dup
+	testexpr(p, "'hello' dup", ['hello', 'hello'])
+	testexpr(p, "(1 2 3) dup", [[1, 2, 3], [1, 2, 3]])
 
-	d = p.eval("4 5 <")
-	print d
-	assert(d == [True])
+	# unpack
+	testexpr(p, "(1 2 3) unpack", [1, 2, 3])
+	# enpack
+	testexpr(p, "1 2 3 enpack", [[1, 2, 3]])
 
-	d = p.eval("5 4 < !")
-	print d
-	assert(d == [True])
+	# int
+	testexpr(p, "3.14 int", [3])
+	testexpr(p, "3.00 int", [3])
 
-	d = p.eval("5 4 >=")
-	print d
-	assert(d == [True])
+	# str
+	testexpr(p, "3.14 str", ["3.14"])
+	testexpr(p, "3.00 str", ["3.0"])
 
-	# create a ref from a string, this will evaluate it and look it up, then return the value
-	d = p.eval("'n' 'ame' + makeref")
-	print d
-	assert(d == ["Jack"])
+	# enumerate
+	testexpr(p, "testlist enumerate", [[(0, 'one'), (1, 'two'), (2, 'three')]], tolist = True)
 
-	# squash
-	d = p.eval("2 2 2 2 * $")
-	print d
-	assert(d == [16])
+	# range
+	testexpr(p, "10 range", [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
 
-	# return the stack as list
-	d = p.eval("(1 2 3)", None)
-	print d
-	assert(d == [[1.0, 2.0, 3.0]])
+	# iteritems
+	testexpr(p, "testdict iteritems", [[('three', 'THREE'), ('two', 'TWO'), ('one', 'ONE')]], tolist = True)
 
-	# create a range, then return the stack as a list
-	d = p.eval("10 range", None)
-	print d
-	assert(d == [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
+	# ceil
+	testexpr(p, "1.5 ceil", [2.0])
+	testexpr(p, "-1.5 ceil", [-1.0])
+	# floor
+	testexpr(p, "1.5 floor", [1.0])
+	testexpr(p, "-1.5 floor", [-2.0])
+
+	# booleans logic
+	testexpr(p, "5 4 <", [False])
+	testexpr(p, "4 5 <")
+	testexpr(p, "5 4 < !")
+	testexpr(p, "5 4 >=")
+
+	# arithmetic
+	testexpr(p, "2 2 2 sum 4 *", [24])
+	testexpr(p, "2 (2 2 sum) 4 prod", [32]) # also tests <"mul" repeat>
+	testexpr(p, "4 *2", [16])
+	testexpr(p, "4 4 **", [256])
+
+	# at
+	testexpr(p, "('jack' 'suzy' 'mary') 2 <>", ["mary"])
+
+	# contains
+	testexpr(p, "('jack' 'suzy' 'mary') 'mary' contains")
+	testexpr(p, "('jack' 'suzy' 'mary') 'sarah' contains", [False])
+
+	# slice, startslice, endslice
+	testexpr(p, "('jack' 'suzy' 'mary' 'sarah') 2 [:", [['mary', 'sarah']])
+	testexpr(p, "('jack' 'suzy' 'mary' 'sarah') 2 :]", [['jack', 'suzy']])
+	testexpr(p, "('jack' 'suzy' 'mary' 'sarah') 1 3 []", [['suzy', 'mary']])
+	# splitat
+	testexpr(p, "('jack' 'suzy' 'mary' 'sarah') 1 splitat", [['jack'], ['suzy', 'mary', 'sarah']])
+	# swap
+	testexpr(p, "('jack' 'suzy' 'mary' 'sarah') 1 splitat swap", [['suzy', 'mary', 'sarah'], ['jack']])
+	# combo double punch
+	testexpr(p, "'jack' 'suzy' 'mary' enpack 1 splitat swap", [['suzy', 'mary'], ['jack']])
 
 	# groupby (and generator) test:
-	d = p.eval("(1 2 3 4 5 6 7 8) 3 {}")
-	# d will be a generator, so check the contents by converting it to a list
-	d = list(d[0])
-	print d
-	assert(d == [[1, 2, 3], [4, 5, 6], [7, 8]])
+	testexpr(p, "(1 2 3 4 5 6 7 8) 3 {}", [[[1, 2, 3], [4, 5, 6], [7, 8]]], tolist = True)
 
-	# filename generation
-	d = p.eval("'static' 'css' ('site-' key '.html' sum) //+")
-	print d
-	assert(d == ["static/css/site-a7c34bd.html"])
+	# filename generation (pathsum and sum)
+	testexpr(p, "'static' 'css' ('site-' key '.html' sum) //+", ["static/css/site-a7c34bd.html"])
 
-	# exception testing
-	def testException(Exc, op):
-		try:
-			print "testing expected exception", Exc, "using code [%s]" % op
-			p.eval(op)
-			assert(False)
-		except Exc as e:
-			print "got expected exception", Exc
-			pass
+	# sum on text
+	testexpr(p, "'static' 'css' ('site-' key '.html' sum) //+", ["static/css/site-a7c34bd.html"])
+
+	# repeat
+	testexpr(p, "1 2 3 4 5 6 7 8 9 10 'add' repeat", [55]) 
 
 	# the cake is a lie
-	testException(pretzyl.InvalidReference, "cake")
+	testexpr(p, "cake", Exc=pretzyl.InvalidReference)
 
 	# these all look like operators, but they should
 	# be rejected since each one has a syntax error.
 	# This results in a lookup failure (ie. KeyError in the environment)
-	testException(pretzyl.InvalidReference, "2 2 *%")
-	testException(pretzyl.InvalidReference, "2 2 **x")
-	testException(pretzyl.InvalidReference, "2 2 ***")
-	testException(pretzyl.InvalidReference, "2 2 +#")
-	testException(pretzyl.InvalidReference, "2 2 $$")
+	testexpr(p, "2 2 *%", 	Exc=pretzyl.InvalidReference)
+	testexpr(p, "2 2 **x", 	Exc=pretzyl.InvalidReference)
+	testexpr(p, "2 2 ***", 	Exc=pretzyl.InvalidReference)
+	testexpr(p, "2 2 +#", 	Exc=pretzyl.InvalidReference)
+	testexpr(p, "2 2 $$", 	Exc=pretzyl.InvalidReference)
 
 	# syntax errors - mismatched brackets
-	testException(pretzyl.NestingException, "( ( )")
-	testException(pretzyl.NestingException, "( ( ) ) )")
+	testexpr(p, "( ( )", 		Exc=pretzyl.NestingException)
+	testexpr(p, "( ( ) ) )", 	Exc=pretzyl.NestingException)
 
 	# the + operator expects two arguments, but there is only one
-	testException(pretzyl.StackUnderflow, "highlander +")
+	testexpr(p, "highlander +", Exc=pretzyl.StackUnderflow)
 	# artificially lower the stack limit, then feed the parser a large stack
 	p.STACKLIMIT = 10
-	testException(pretzyl.StackOverflow, "0 1 2 3 4 5 6 7 8 9 10")
+	testexpr(p, "0 1 2 3 4 5 6 7 8 9 10", Exc=pretzyl.StackOverflow)
 	# artificially lower the stack depth, then feed the parser a deep nesting of scopes
 	p.STACKDEPTH = 5
-	testException(pretzyl.RecursionOverflow, "( ( ( ( ( ( ) ) ) ) ) )")
-	# artificially lower the iteration limit, then feed the parser a big squash
-	p.INFLIMIT = 5
-	testException(pretzyl.IterationOverflow, "0 1 2 3 4 5 6 sum")
+	testexpr(p, "( ( ( ( ( ( ) ) ) ) ) )", Exc=pretzyl.RecursionOverflow)
+	
+	## artificially lower the iteration limit, then feed the parser a big squash
+	#p.INFLIMIT = 5
+	#testException(p, pretzyl.IterationOverflow, "0 1 2 3 4 5 6 sum")
 
 	return 0
 
